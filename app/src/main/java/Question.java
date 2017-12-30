2ea460b2f2f517f26be1fee6a77ec4fc03322784
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-30 10:18:49 +0100

    Add member variables and construtor

--- app/src/main/java/Question.java
+++ app/src/main/java/Question.java
@@ -3,4 +3,12 @@
  */
 
 public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
 }
