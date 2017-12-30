cc303cde0ad2f1312eaa468e1fd70ede6cdcc1f5
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-30 17:52:22 +0100

    Wire up the NEXT button

--- app/src/main/java/com/example/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/geoquiz/QuizActivity.java
@@ -57,5 +57,15 @@ public class QuizActivity extends AppCompatActivity {
                 toast.show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
+        });
     }
 }
